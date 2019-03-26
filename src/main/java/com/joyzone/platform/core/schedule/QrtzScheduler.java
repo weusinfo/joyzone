f26db4a8de3524a6318945bba9042e7b590fc582
liutao <hebing_o@hotmail.com>
2019-03-26 18:05:47 +0800

    暂时屏蔽

--- src/main/java/com/joyzone/platform/core/schedule/QrtzScheduler.java
+++ src/main/java/com/joyzone/platform/core/schedule/QrtzScheduler.java
@@ -54,10 +54,10 @@ public class QrtzScheduler {
         return scheduler;
     }
 
-    @Bean
-    public JobDetail jobDetail() {
-        return newJob().ofType(SampleJob.class).storeDurably().withIdentity(JobKey.jobKey("Qrtz_Job_Detail")).withDescription("Invoke Sample Job service...").build();
-    }
+//    @Bean
+//    public JobDetail jobDetail() {
+//        return newJob().ofType(SampleJob.class).storeDurably().withIdentity(JobKey.jobKey("Qrtz_Job_Detail")).withDescription("Invoke Sample Job service...").build();
+//    }
 
     @Bean
     public Trigger trigger(JobDetail job) {
