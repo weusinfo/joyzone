47a1d9afcd878479edc17048de36e69a47be3547
louis.he <hebing_o@hotmail.com>
2019-03-30 00:47:21 +0800

    adjust swagger shown style

--- src/main/java/com/joyzone/platform/config/SwaggerConfig.java
+++ src/main/java/com/joyzone/platform/config/SwaggerConfig.java
@@ -1,12 +1,10 @@
 package com.joyzone.platform.config;
 
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,9 +13,7 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.List;
-
 import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
@@ -30,7 +26,7 @@ public class SwaggerConfig {
             .apiInfo(apiInfo())
             .select()
             //加了ApiOperation注解的类，才生成接口文档
-            .apis(RequestHandlerSelectors.basePackage("com.joyzone.platform.module.admin.controller"))
+            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
             .paths(PathSelectors.any())
             .build()
             .genericModelSubstitutes(ResponseEntity.class)
