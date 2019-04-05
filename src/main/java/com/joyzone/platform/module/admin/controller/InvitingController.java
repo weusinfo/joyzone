744018268c0b848e62054e84bd17987779ee7669
Ryan <17720547006@163.com>
2019-04-05 23:58:42 +0800

    增加评论及邀约等相关代码

---
+++ src/main/java/com/joyzone/platform/module/admin/controller/InvitingController.java
@@ -0,0 +1,61 @@
+package com.joyzone.platform.module.admin.controller;
+
+
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import com.joyzone.platform.common.utils.Constants;
+import com.joyzone.platform.common.utils.R;
+import com.joyzone.platform.core.model.InvitingModel;
+import com.joyzone.platform.core.service.InvitingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.util.List;
+
+@RestController
+@RequestMapping("/inviting")
+@Api(value="后台邀约管理",tags="1.0")
+public class InvitingController {
+
+    @Autowired
+    private InvitingService invitingService;
+
+    /**
+     * Mr.Gx
+     */
+    @ApiOperation("获取邀请列表")
+    @GetMapping("getInvitingList")
+    public R getInvitingList(InvitingModel invitingModel){
+        return invitingService.getInvitingList(invitingModel);
+    }
+
+    /**
+     * Mr.Gx
+     */
+    @ApiOperation("获取邀请详情信息")
+    @GetMapping("findById/{id}")
+    public R findById(@PathVariable Long id){
+        return invitingService.findById(id);
+    }
+
+    @GetMapping("/exportInvitingXls")
+    @ApiOperation("发起邀请清单导出")
+    public void exportInvitingXls(InvitingModel invitingModel, HttpServletResponse response) throws Exception{
+        response.setHeader("content-Type", "application/vnd.ms-excel");
+        response.setHeader("Content-Disposition",
+                "attachment;filename=" + URLEncoder.encode(Constants.JOY_INVITING, "UTF-8") + ".xls");
+        response.setCharacterEncoding("UTF-8");
+        List<InvitingModel> list = invitingService.selectInvitingList(invitingModel);
+        ExportParams params = new ExportParams(Constants.JOY_INVITING, Constants.JOY_INVITING);
+        Workbook workbook = ExcelExportUtil.exportExcel(params, InvitingModel.class, list);
+        workbook.write(response.getOutputStream());
+    }
+}
--- src/main/java/com/joyzone/platform/module/admin/controller/OrderController.java
+++ src/main/java/com/joyzone/platform/module/admin/controller/OrderController.java
@@ -1,16 +1,24 @@
 package com.joyzone.platform.module.admin.controller;
 
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import com.joyzone.platform.common.utils.Constants;
 import com.joyzone.platform.common.utils.R;
 import com.joyzone.platform.core.dto.OrderDto;
 import com.joyzone.platform.core.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -33,7 +41,7 @@ public class OrderController {
      * Mr.Gx
      */
     @GetMapping("findById/{id}")
-    @ApiOperation("后台订单列表")
+    @ApiOperation("后台订单详情")
     public R findById(@PathVariable Long id){
         return orderService.findById(id);
     }
@@ -42,8 +50,21 @@ public class OrderController {
      * Mr.Gx
      */
     @GetMapping("getTeamUsers/{teamId}/{pageNum}/{pageSize}")
-    @ApiOperation("后台订单列表")
+    @ApiOperation("后台订单获取参加人员列表")
     public R getTeamUsers(@PathVariable Long teamId,@PathVariable Integer pageNum,@PathVariable Integer pageSize){
         return orderService.getTeamUsers(teamId,pageNum,pageSize);
+    }
+
+    @GetMapping("/exportOrderXls")
+    @ApiOperation("订单清单导出")
+    public void exportShopCouponXls(OrderDto orderDto, HttpServletResponse response) throws Exception{
+        response.setHeader("content-Type", "application/vnd.ms-excel");
+        response.setHeader("Content-Disposition",
+                "attachment;filename=" + URLEncoder.encode(Constants.JOY_ORDER, "UTF-8") + ".xls");
+        response.setCharacterEncoding("UTF-8");
+        List<OrderDto> list = orderService.selectOrderList(orderDto);;
+        ExportParams params = new ExportParams(Constants.JOY_ORDER, Constants.JOY_ORDER);
+        Workbook workbook = ExcelExportUtil.exportExcel(params, OrderDto.class, list);
+        workbook.write(response.getOutputStream());
     }
 }
