985a4d3c20eb9f8aaf33db8754ada4c564f386d4
ZhangYu <1689528405@qq.com>
2019-04-21 21:27:37 +0800

    体验券列表

--- src/main/java/com/joyzone/platform/core/dto/UserJoinCouponDto.java
+++ src/main/java/com/joyzone/platform/core/dto/UserJoinCouponDto.java
@@ -1,20 +1,35 @@
 package com.joyzone.platform.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.joyzone.platform.core.model.BaseModel;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by zy on 2019/4/20.
  */
 @ApiModel("参加信息DTO")
+@Configuration
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
 public class UserJoinCouponDto extends BaseModel {
+
+    @ApiModelProperty("体验券ID")
+    private Long couponId;
 
     @ApiModelProperty("用户ID")
     private Long userId;
 
     @ApiModelProperty("用户头像")
     private String userHeadImg;
+
+    public Long getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(Long couponId) {
+        this.couponId = couponId;
+    }
 
     public Long getUserId() {
         return userId;
