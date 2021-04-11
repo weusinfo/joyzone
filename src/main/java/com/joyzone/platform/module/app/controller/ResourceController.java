bf34c501c4bbeb4bd7fc8d244299fbb69c89ba2e
hebingooo <57800324+hebingooo@users.noreply.github.com>
2021-04-11 17:44:57 +0800

    Add files via upload
    adapter to ios wechar share

---
+++ src/main/java/com/joyzone/platform/module/app/controller/ResourceController.java
@@ -0,0 +1,28 @@
+package com.joyzone.platform.module.app.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ResourceController {
+	
+	@RequestMapping(name = "/apple-app-site-association",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String getAppJson() {
+		String json = "{\r\n" + 
+				"    \"applinks\": {\r\n" + 
+				"        \"apps\": [],\r\n" + 
+				"        \"details\": [\r\n" + 
+				"            {\r\n" + 
+				"                \"appID\": \"4WRFSUX5RH.chaifuwei.Quwan\",\r\n" + 
+				"                \"paths\": [\"*\"]\r\n" + 
+				"            }\r\n" + 
+				"        ]\r\n" + 
+				"    }\r\n" + 
+				"}";
+		return json;
+	}
+
+}
